1265e2064c42526f71aebb69949e34c899acc5c3
aadennis <dennis.circ@gmail.com>
2016-10-14 23:02:21 +0100

    Basic IIS service

---
+++ HelloAcmeService/HelloService.cs
@@ -0,0 +1,7 @@
+﻿namespace HelloAcmeService {
+    public class HelloService : IHelloService {
+        public string GetData(int value) {
+            return string.Format("You entered: {0}", value);
+        } 
+    }
+}
---
+++ HelloServiceClient/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloServiceAClient
+{
+    class Program
+    {
+        static void Main(string[] args)         {
+            var clientProxy = new ServiceReference1.HelloServiceClient();
+            var msg = clientProxy.GetData(101);
+            Console.WriteLine(msg);
+            Console.ReadKey();
+        }
+    }
+}
