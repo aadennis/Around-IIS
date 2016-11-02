ffd05b4ef1adec055fb6d2f96ad78a7e71f4d069
aadennis <dennis.circ@gmail.com>
2016-11-02 20:54:35 +0000

    Proving out basic version of the YouTube solution, again
    Proving out basic version of the YouTube solution, again

--- HelloAcmeClient/Program.cs
+++ HelloAcmeClient/Program.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using HelloAcmeService;
 
-namespace HelloServiceClient
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var clientProxy = new HelloAcmeClient.ServiceReference1.HelloServiceClient();
+namespace HelloServiceClient {
+    class Program {
+        static void Main(string[] args) {
+            var clientProxy = new HelloAcmeClient.ServiceReference.HelloServiceClient();
             var msg = clientProxy.GetData(101);
             Console.WriteLine(msg);
             Console.ReadKey();
--- HelloAcmeService/HelloService.cs
+++ HelloAcmeService/HelloService.cs
@@ -1,8 +1,7 @@
-﻿namespace HelloAcmeService
-{
+﻿namespace HelloAcmeService {
     public class HelloService : IHelloService {
         public string GetData(int value) {
-            return string.Format("You entered: {0}", value);
+            return string.Format("You pretty much entered: {0}", value);
         }
     }
 }
